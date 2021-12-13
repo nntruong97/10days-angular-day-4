ef26e5fdd5b74c0342c7ae33d457a762b2eee270
nntruong97 <nguyennhattruongspkt@gmail.com>
2021-12-13 22:13:21 +0700

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# angular-ivy-rt2nry
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/angular-ivy-rt2nry)
---
+++ src/app/hello.component.ts
@@ -0,0 +1,17 @@
+import { Component, Input } from '@angular/core';
+
+@Component({
+  selector: 'hello',
+  template: `<h1>Hello {{name}}!</h1>`,
+  styles: [`h1 { font-family: Lato; }`],
+})
+export class HelloComponent {
+  @Input() name: string;
+
+  ngOnInit() {
+    console.log('init');
+  }
+  ngOnDestroy() {
+    console.log('Destroy');
+  }
+}
